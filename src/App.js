284d8f20b96168842918c8852d1e74be7234df0e
gboog <griffinblotner@gmail.com>
2023-10-24 12:36:18 -0400

    test 1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <nav>
+          <ul>
+            <li>
+              Home
+            </li>
+          </ul>
+        </nav>
       </header>
     </div>
   );
