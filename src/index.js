816dbd62d894fdf98d4d243820fa411639206ac8
gboog <griffinblotner@gmail.com>
2023-11-15 15:46:30 -0500

    Added Favicon

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Button variant="primary">Primary</Button>{' '}
 
   </React.StrictMode>
 );
